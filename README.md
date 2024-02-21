0399b0fc6f58884ed39883996d48fd8926c70e30
NShunsaku <shun.rowing@gmail.com>
2024-02-21 16:05:48 +0900

    ver. 20240221

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SAMV71_tutorial
+
+This is the tutorial for SAMV71 microcontroller.
---
+++ do_git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$today = Get-Date -Format "yyyyMMdd"
+git add .
+git commit -m "ver. ${today}"
+git push -uf origin main
